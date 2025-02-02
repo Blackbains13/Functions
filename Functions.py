3bb030ee397d52bf9ff716c1b06ee6cc984f42cc
Blackbains13 <kwasiblackbains@gmail.com>
2025-02-02 13:06:12 +0000

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,14 @@
+
+def Kwasi():
+    print("Bonjour,Kwasi")
+    print("Good Morning,Abhiraj")
+    print("Hola,Jeffin")
+    print("Namaste, Akash")
+
+def Nelson():
+    print("Zdravo,Perisic")
+    print("Olà,Bernado")
+    print("hej,Hojlund")
+
+Kwasi()
+Nelson()
