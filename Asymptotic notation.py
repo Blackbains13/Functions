10e80f6d6aa1b4b90ca274796f0d12f98de5bc88
Blackbains13 <kwasiblackbains@gmail.com>
2025-04-20 12:26:10 +0000

    Add files via upload

---
+++ Asymptotic notation.py
@@ -0,0 +1,17 @@
+def printnumber(n):
+    literation=0
+    print("the total number enterned my user is",n)
+    literation+=1
+    print("total literation done by computer is",literation,)
+printnumber(10)
+printnumber(200000)
+printnumber(200000)
+printnumber(200000)
+printnumber(200000)
+printnumber(200000)
+printnumber(200000)
+printnumber(200000)
+printnumber(200000)
+printnumber(200000)
+printnumber(200000)
+print("\n with any 'n' in time taken by the computer will not change")
